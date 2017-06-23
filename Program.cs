8d18a32cc19c9b5b40668d6abbc16b936ba0fb4c
Wayne Burris <waterseeker@hotmail.com>
2017-06-22 19:25:59 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Exercise7
+{
+    public static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num1= Convert.ToInt32(Console.ReadLine());//takes user input and converts it to an int from the string
+ 
+        Console.Write("Enter another number: ");
+        int num2= Convert.ToInt32(Console.ReadLine());
+   
+        Console.WriteLine("{0} + {1} = {2}", num1, num2, num1+num2); //adds nums
+        Console.WriteLine("{0} - {1} = {2}", num1, num2, num1-num2); //subtracts nums
+        Console.WriteLine("{0} x {1} = {2}", num1, num2, num1*num2); //multiplies nums
+        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1/num2); //divides nums
+        Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1%num2); //modulus nums
+   }
+}
